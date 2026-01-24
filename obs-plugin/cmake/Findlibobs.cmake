fbf3e943bd247493ddd3475ebce18d36c68fa962
Yuna <waseinschei@katzenzungen.lol>
2026-01-24 22:03:41 +0100

    CMake: prefer CONFIG for libobs; add robust Unix fallback with pkg-config; improve Windows linking

--- obs-plugin/cmake/Findlibobs.cmake
+++ obs-plugin/cmake/Findlibobs.cmake
@@ -1,61 +1,77 @@
-# Fallback finder for libobs when a config package is not present (e.g., Windows choco install).
-
- set(_pf64 "$ENV{ProgramW6432}")
- set(_pf   "$ENV{ProgramFiles}")
- 
- set(_OBS_INC_HINTS)
- if(NOT _pf64 STREQUAL "")
-     list(APPEND _OBS_INC_HINTS "${_pf64}/obs-studio/include")
- endif()
- if(NOT _pf STREQUAL "")
-     list(APPEND _OBS_INC_HINTS "${_pf}/obs-studio/include")
- endif()
-
-find_path(LIBOBS_INCLUDE_DIR
-    NAMES obs.h
-    PATHS ${_OBS_INC_HINTS}
-)
-
- set(_OBS_LIB_HINTS)
- if(NOT _pf64 STREQUAL "")
-     list(APPEND _OBS_LIB_HINTS
-         "${_pf64}/obs-studio/bin/64bit"
-         "${_pf64}/obs-studio/obs-plugins/64bit"
-     )
- endif()
- if(NOT _pf STREQUAL "")
-     list(APPEND _OBS_LIB_HINTS
-         "${_pf}/obs-studio/bin/64bit"
-         "${_pf}/obs-studio/obs-plugins/64bit"
-     )
- endif()
-
-find_library(LIBOBS_IMPLIB
-    NAMES obs libobs obs.lib libobs.lib
-    PATHS
-        ${_OBS_LIB_HINTS}
-        "${_pf64}/obs-studio/lib/64bit"
-        "${_pf}/obs-studio/lib/64bit"
-)
-
-find_file(LIBOBS_DLL
-    NAMES libobs.dll obs.dll
-    PATHS ${_OBS_LIB_HINTS}
-)
+# Fallback finder for libobs when a config package is not present.
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libobs
-    REQUIRED_VARS LIBOBS_INCLUDE_DIR LIBOBS_IMPLIB)
 
-if(libobs_FOUND)
-    add_library(OBS::libobs SHARED IMPORTED)
-    if(LIBOBS_DLL)
-        set_target_properties(OBS::libobs PROPERTIES
-            IMPORTED_LOCATION "${LIBOBS_DLL}"
+if(WIN32)
+    # Windows: locate headers, import library and optional DLL
+    set(_pf64 "$ENV{ProgramW6432}")
+    set(_pf   "$ENV{ProgramFiles}")
+
+    set(_OBS_INC_HINTS)
+    if(NOT _pf64 STREQUAL "")
+        list(APPEND _OBS_INC_HINTS "${_pf64}/obs-studio/include")
+    endif()
+    if(NOT _pf STREQUAL "")
+        list(APPEND _OBS_INC_HINTS "${_pf}/obs-studio/include")
+    endif()
+
+    find_path(LIBOBS_INCLUDE_DIR NAMES obs.h PATHS ${_OBS_INC_HINTS})
+
+    set(_OBS_LIB_HINTS)
+    if(NOT _pf64 STREQUAL "")
+        list(APPEND _OBS_LIB_HINTS
+            "${_pf64}/obs-studio/bin/64bit"
+            "${_pf64}/obs-studio/obs-plugins/64bit"
+            "${_pf64}/obs-studio/lib/64bit"
         )
     endif()
-    set_target_properties(OBS::libobs PROPERTIES
-        IMPORTED_IMPLIB "${LIBOBS_IMPLIB}"
-        INTERFACE_INCLUDE_DIRECTORIES "${LIBOBS_INCLUDE_DIR}"
-    )
+    if(NOT _pf STREQUAL "")
+        list(APPEND _OBS_LIB_HINTS
+            "${_pf}/obs-studio/bin/64bit"
+            "${_pf}/obs-studio/obs-plugins/64bit"
+            "${_pf}/obs-studio/lib/64bit"
+        )
+    endif()
+
+    find_library(LIBOBS_IMPLIB NAMES obs libobs obs.lib libobs.lib PATHS ${_OBS_LIB_HINTS})
+    find_file(LIBOBS_DLL NAMES libobs.dll obs.dll PATHS ${_OBS_LIB_HINTS})
+
+    find_package_handle_standard_args(libobs REQUIRED_VARS LIBOBS_INCLUDE_DIR LIBOBS_IMPLIB)
+
+    if(libobs_FOUND)
+        add_library(OBS::libobs SHARED IMPORTED)
+        if(LIBOBS_DLL)
+            set_target_properties(OBS::libobs PROPERTIES IMPORTED_LOCATION "${LIBOBS_DLL}")
+        endif()
+        set_target_properties(OBS::libobs PROPERTIES
+            IMPORTED_IMPLIB "${LIBOBS_IMPLIB}"
+            INTERFACE_INCLUDE_DIRECTORIES "${LIBOBS_INCLUDE_DIR}"
+        )
+    endif()
+else()
+    # Unix: try pkg-config, else search common include/lib paths
+    find_package(PkgConfig QUIET)
+    if(PKG_CONFIG_FOUND)
+        pkg_check_modules(LIBOBS_PC QUIET libobs)
+    endif()
+
+    if(LIBOBS_PC_FOUND)
+        # Use pkg-config results
+        set(LIBOBS_INCLUDE_DIR "${LIBOBS_PC_INCLUDE_DIRS}")
+        find_library(LIBOBS_LIBRARY NAMES obs libobs PATHS ${LIBOBS_PC_LIBRARY_DIRS} /usr/lib /usr/local/lib /usr/lib/x86_64-linux-gnu)
+    else()
+        # Fallback paths
+        find_path(LIBOBS_INCLUDE_DIR NAMES obs.h PATHS /usr/include/obs /usr/local/include/obs /usr/include /usr/local/include)
+        find_library(LIBOBS_LIBRARY NAMES obs libobs PATHS /usr/lib /usr/local/lib /usr/lib/x86_64-linux-gnu)
+    endif()
+
+    find_package_handle_standard_args(libobs REQUIRED_VARS LIBOBS_INCLUDE_DIR LIBOBS_LIBRARY)
+
+    if(libobs_FOUND)
+        add_library(OBS::libobs SHARED IMPORTED)
+        set_target_properties(OBS::libobs PROPERTIES
+            IMPORTED_LOCATION "${LIBOBS_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${LIBOBS_INCLUDE_DIR}"
+        )
+    endif()
 endif()
