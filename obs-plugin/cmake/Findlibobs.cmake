b82684832652dbd10dc17cac94ab4220f4058e4c
Yuna <waseinschei@katzenzungen.lol>
2026-01-24 22:01:12 +0100

    CMake: find Windows import lib and DLL; set IMPORTED_IMPLIB/LOCATION for libobs

--- obs-plugin/cmake/Findlibobs.cmake
+++ obs-plugin/cmake/Findlibobs.cmake
@@ -30,19 +30,32 @@ find_path(LIBOBS_INCLUDE_DIR
      )
  endif()
 
-find_library(LIBOBS_LIBRARY
-    NAMES obs libobs
+find_library(LIBOBS_IMPLIB
+    NAMES obs libobs obs.lib libobs.lib
+    PATHS
+        ${_OBS_LIB_HINTS}
+        "${_pf64}/obs-studio/lib/64bit"
+        "${_pf}/obs-studio/lib/64bit"
+)
+
+find_file(LIBOBS_DLL
+    NAMES libobs.dll obs.dll
     PATHS ${_OBS_LIB_HINTS}
 )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(libobs
-    REQUIRED_VARS LIBOBS_INCLUDE_DIR LIBOBS_LIBRARY)
+    REQUIRED_VARS LIBOBS_INCLUDE_DIR LIBOBS_IMPLIB)
 
 if(libobs_FOUND)
-    add_library(OBS::libobs UNKNOWN IMPORTED)
+    add_library(OBS::libobs SHARED IMPORTED)
+    if(LIBOBS_DLL)
+        set_target_properties(OBS::libobs PROPERTIES
+            IMPORTED_LOCATION "${LIBOBS_DLL}"
+        )
+    endif()
     set_target_properties(OBS::libobs PROPERTIES
-        IMPORTED_LOCATION "${LIBOBS_LIBRARY}"
+        IMPORTED_IMPLIB "${LIBOBS_IMPLIB}"
         INTERFACE_INCLUDE_DIRECTORIES "${LIBOBS_INCLUDE_DIR}"
     )
 endif()
